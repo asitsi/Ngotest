eb46014f5fe1d59e8d35e8e6c72ea2df3ba5db88
asitsi <41507591+asitsi@users.noreply.github.com>
2021-06-20 00:50:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <ScrollRoute />
-      <Header />
+      
       <Navbar />
       <Switch>
         <Route path="/Ngotest/" exact component={Home} />
